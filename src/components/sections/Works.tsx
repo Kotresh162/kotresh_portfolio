35335cc554dd6d1f8b4c116bd0682456f53b502a
Kotresh162 <kotreshsh162@gmail.com>
2025-07-21 00:23:02 +0530

    css

--- src/components/sections/Works.tsx
+++ src/components/sections/Works.tsx
@@ -77,7 +77,7 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap gap-12">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
